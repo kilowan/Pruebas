88507b622db3c4ae38ac2ee698ba6378f9790d74
evoblackshark <kilowan2@gmail.com>
2019-07-26 10:49:19 +0200

    update code

--- hello-backbone-master/hello.js
+++ hello-backbone-master/hello.js
@@ -1,61 +1,92 @@
-(function($) {
-
-    var pruebas = Backbone.Marionette.View.extend(
-    {
-        el : '#UserInput',
-        initialize: function () {
-            this.helloListView = new HelloListView();
-        },
-        events: {
-            'click button': 'addToHelloCollection'
-        },
-        addToHelloCollection: function () {
-            var hello = new Hello({
-                name: this.$('#name').val(),
-                age: this.$('#age').val()
-            });
-            this.helloListView.collection.add(hello);
+    //01 - Modelo de usuario
+    const Usuario = Backbone.Model.extend({
+        defaults: {
+            nombre: "",
+            edad: 0
         }
-        });
-    var Hello = Backbone.Model.extend(
+    });
+    //02 - Coleccion de modelos
+    const Usuarios = Backbone.Collection.extend({
+        Model: Usuario
+    });
+
+    //03 - Vista que captura el patron del usuario
+    const UsuarioView = Backbone.Marionette.View.extend({
+        tagName: "li",
+        template: "#userView"
+    });
+
+    //05 - Coleccion que coge la vista de usuario (si existe) o la vista vacia
+    const UsuariosView = Backbone.Marionette.CollectionView.extend({
+        //tagName: "p",
+        childView: UsuarioView,
+    });
+
+    //06 - Vista que captura los datos del formulario
+    const FormView = Backbone.Marionette.View.extend({
+        //tagName: "div",
+        className: "form-view",
+        template: "#formView",
+        events: {
+            'click button': 'crearNuevoUsuario'
+        },
+        ui: {
+            nombre: '#nombre',
+            edad: '#edad'
+        },
+        crearNuevoUsuario: function () {
+            this.collection.push({
+                nombre: this.ui.nombre.val(),
+                edad: this.ui.edad.val()
+            });
+            this.ui.nombre.val("");
+            this.ui.edad.val("");
+        }
+
+    });
+
+    //07 - Vista que captura los datos de la plantilla containerview y renderiza las vistas hijas
+    const ContainerView = Backbone.Marionette.View.extend(
         {
-            initialize: function () {
-                this.name = 'name',
-                    this.age = 'age'
+            //plantilla html
+            template: '#containerView',
+            //elemento a pintar
+            //tagName: "div",
+            //zona(s) donde pintara los datos
+            regions: {
+                form: '#form',
+                list: '#list'
+            },
+            //constructor
+            initialize: function (options) {
+                this.formView = new FormView({
+                    collection: options.usuarios
+                });
+                this.usuariosView = new UsuariosView({
+                    collection: options.usuarios
+                });
+            },
+            //renderizador
+            onRender() {
+                this.showChildView('form', this.formView);
+                this.showChildView('list', this.usuariosView);
+            }
+        })
+
+    //08 - app principal
+    const UsuariosApp = Backbone.Marionette.Application.extend(
+        {
+            //zona donde se cargara el codigo
+            region: '#content',
+            //constructor
+            initialize: function (options) {
+                this.containerView = new ContainerView(options);
+                this.showView(this.containerView);
             }
         });
-    var Data = Backbone.Marionette.View.extend(
-    {
-		tagName : 'li',
-		render : function() {
-			$(this.el).html('Hello World, my name is ' + this.model.get('name') + ' and I have ' + this.model.get('age') + ' years old');
-			return this;
-		}
-	});
-    var HelloList = Backbone.Collection.extend(
-    {
-		model : Hello
-	});
-    var HelloListView = Backbone.Marionette.View.extend(
-    {
-        el: '#HelloList',
-		initialize : function() {
-			_.bindAll(this, 'render', 'appendToHelloUL');
-			this.collection = new HelloList();
-			this.collection.bind('add', this.appendToHelloUL);
-		},
-		render : function(){
-			$.each(this.collection.models, function(blablaModel){
-				self.appendToHelloUL(blablaModel);
-			});
-		},
-		appendToHelloUL : function(blablaModel) {
-			var data = new Data({
-				model : blablaModel
-			});
-			$(this.el).append(data.render().el);
-		}
-    });
-    new pruebas();
 
-})(jQuery);
+    //09 - llamada a la app
+    $(function () {
+        var app = new UsuariosApp({ usuarios: new Usuarios() });
+        app.start();
+    });
