845c3cfea09c4a43349fd045cc8cb7522693ac74
evoblackshark <kilowan2@gmail.com>
2019-08-06 09:28:03 +0200

    add code

---
+++ Otros/SHP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+
+namespace SHP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientContext context = new ClientContext("http://pruebabeca2019/beca/kilowan/");
+            Web web = context.Web;
+            context.Load(web);
+            context.ExecuteQuery();
+            Console.WriteLine(web.Title);
+        }
+    }
+}
